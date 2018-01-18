61790285f36feae0bd2ca8d0e7a36218887100bd
Manu160296 <fmanuela499@gmail.com>
2018-01-18 17:37:30 -0500

    arreglando indentacion

--- js/app.js
+++ js/app.js
@@ -1,71 +1,70 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDW1o9U4yoqCKqmc_rZZJfULlQpcrE33m4",
-    authDomain: "producto-2ce12.firebaseapp.com",
-    databaseURL: "https://producto-2ce12.firebaseio.com",
-    projectId: "producto-2ce12",
-    storageBucket: "producto-2ce12.appspot.com",
-    messagingSenderId: "846954295488"
-  };
-  firebase.initializeApp(config);
+  apiKey: 'AIzaSyDW1o9U4yoqCKqmc_rZZJfULlQpcrE33m4',
+  authDomain: 'producto-2ce12.firebaseapp.com',
+  databaseURL: 'https://producto-2ce12.firebaseio.com',
+  projectId: 'producto-2ce12',
+  storageBucket: 'producto-2ce12.appspot.com',
+  messagingSenderId: '846954295488'
+};
+firebase.initializeApp(config);
 // login with google:
-  var user = null ; 
-  var usuariosConectados = null;
-  //conexion a la base de datos:
-  var database = firebase.database();
-  // llave unica por conexion :
-  var conectadoKey = '';
-  var $loginBtn = $('#start-login');
-  $loginBtn.on('click', googleLogin) ;
+var user = null ;
+var usuariosConectados = null;
+// conexion a la base de datos:
+var database = firebase.database();
+// llave unica por conexion :
+var conectadoKey = '';
+var $loginBtn = $('#start-login');
+$loginBtn.on('click', googleLogin) ;
 
-  function googleLogin() {
-      var provider = new firebase.auth.GoogleAuthProvider();
+function googleLogin() {
+  var provider = new firebase.auth.GoogleAuthProvider();
 
-      // funcion de la docummentacion :
+  //  funcion de la docummentacion :
 
-      firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(function(result){
-          //guardamos el usuario que nos trae result:
-          user = result.user;
-          //mostramos su contenido:
-          console.log(user);
-          //ocultamos el div de login :
-       //   $('#login').fadeOut();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function(result) {
+      // guardamos el usuario que nos trae result:
+      user = result.user;
+      // mostramos su contenido:
+      console.log(user);
+      // ocultamos el div de login :
+      //   $('#login').fadeOut();
 
-          initAApp();
-        // redireccion de página :
-        window.location.href = 'views/home.html'
-      })
-  }
+      initAApp();
+      // redireccion de página :
+      window.location.href = 'views/home.html';
+    });
+}
 
- function initAApp () {
-     // referencia a una tabla en base de datos :
-     usuariosConectados = database.ref('/connected');
+function initAApp() {
+  // referencia a una tabla en base de datos :
+  usuariosConectados = database.ref('/connected');
 
-     // 2 parámetros, el 2do si no tiene display name ,toma el email:
+  // 2 parámetros, el 2do si no tiene display name ,toma el email:
 
-     login(user.uid, user.displayName || user.email);
+  login(user.uid, user.displayName || user.email);
+}
 
- }
+function login(uid, name) {
+  // se crea un objeto en la base de datos y se guarda en la referencia:
 
- function login(uid, name) {
-     // se crea un objeto en la base de datos y se guarda en la referencia:
+  var conectado = usuariosConectados.push({
+    uid: uid,
+    name: name
+  });
 
-     var conectado = usuariosConectados.push({
-         uid:uid,
-         name: name 
-     });
+  // identificador unico de registro :
 
-     // identificador unico de registro :
+  conectadoKey = conectado.key;
+}
 
-     conectadoKey = conectado.key;
- }
+// en vista home :
 
- //en vista home : 
- 
- var logOut = $('.log-out-js');
+var logOut = $('.log-out-js');
 logOut.on('click', signOut);
 function signOut() {
   firebase.auth().onAuthStateChanged(function(user) {
