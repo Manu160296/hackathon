c0d16a4a7c8660b1c3afc30b2f84056ad069bf34
diana1418 <a20166303@pucp.edu.pe>
2018-01-19 09:00:07 -0500

    solution to problem with firebase

---
+++ js/chat.js
@@ -0,0 +1,25 @@
+var txtNombre = document.getElementById('nombre');
+var txtMensaje = document.getElementById('mensaje');
+var btnEnviar = document.getElementById('btnEnviar');
+var chatUl = document.getElementById('chatUl');
+btnEnviar.addEventListener("click", function(event) {
+  event.preventDefault();
+var nombre = txtNombre.value;
+var mensaje = txtMensaje.value;
+var html = "<li><b>"+nombre+": </b>"+mensaje+"</li>";
+chatUl.innerHTML += html;
+firebase.database().ref('chat').push({
+    name: nombre,
+    menssaje: mensaje
+ });
+});
+firebase.database().ref('chat').on('value', function(snapshot) {
+    var html = '';
+    snapshot.forEach(function (e) {
+    var element = e.val();
+    var nombre = element.name;
+    var mensaje = element.menssage;
+    html+= "<li><b>"+nombre+": </b>"+mensaje+"</li>";
+    });
+    chatUl.innerHTML = html;
+});
