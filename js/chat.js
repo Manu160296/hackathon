94ee3be66a67fd435ee722aeeb6a9fa1a57cde41
diana1418 <a20166303@pucp.edu.pe>
2018-01-19 10:17:24 -0500

    changes in chat.css y chat.html

--- js/chat.js
+++ js/chat.js
@@ -3,26 +3,25 @@ var txtMensaje = document.getElementById('mensaje');
 var btnEnviar = document.getElementById('btnEnviar');
 var chatUl = document.getElementById('chatUl');
 
-btnEnviar.addEventListener("click", function() {
+btnEnviar.addEventListener('click', function() {
   var nombre = txtNombre.value;
   var mensaje = txtMensaje.value;
   var html = "<li><b>"+nombre+": </b>"+mensaje+"</li>";
   chatUl.innerHTML += html;
 
   firebase.database().ref('chat').push({
-      name: nombre,
-      menssaje: mensaje
+    name: nombre,
+    menssaje: mensaje
   });
 });
 
-firebase.database().ref('chat')
-.on('value', function(snapshot) {
-    var html = '';
-    snapshot.forEach(function (e) {
-      var element = e.val();
-      var nombre = element.name;
-      var mensaje = element.menssaje;
-      html+= "<li><b>"+nombre+": </b>"+mensaje+"</li>";
-    });
-    chatUl.innerHTML = html;
+firebase.database().ref('chat').on('value', function(snapshot) {
+  var html = '';
+  snapshot.forEach(function(e) {
+    var element = e.val();
+    var nombre = element.name;
+    var mensaje = element.menssaje;
+    html += "<li><b>"+nombre+": </b>"+mensaje+"</li>";
+  });
+  chatUl.innerHTML = html;
 });
